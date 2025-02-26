e5329a69ff6ddd53a6de328c009db612421bfd8d
Axile24 <amokrane.n@icloud.com>
2025-02-26 13:52:30 +0100

    nav x knappen works

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -5,29 +5,30 @@ import { menuLinks } from "../Componets/Nav/MenuLinks";
 import MenuNav from "../Componets/Nav/MenuNav";
 import "../styles/Nav-css/nav.scss";
 
-//TODO: QUE JE FAIS?  QUE va se passer qaund je presse le X?
+
 
 const Nav = () => {
   const [isVisible, setIsVisible] = useState(true);
   const navigate = useNavigate();
+
   const handleClose = () => {
-    //window.history.back();
-    //window.close();
-    navigate("/");
-
-    setIsVisible(false);
-
-    console.log("Back to LANDING PAGE");
+    navigate(-1); // Gå tillbaka en sida i historiken eller man ockå göra navigate("/"); för att gå till startsidan
+    setIsVisible(false); // Dölj navigationsmenyn
   };
 
-  return isVisible ? (
+  return isVisible && (
     <nav className="nav">
-      <img src={CloseIcon} alt="Close" onClick={handleClose} />
+      <img 
+        src={CloseIcon} 
+        alt="Close" 
+        onClick={handleClose} 
+        className="close-icon" 
+      />
       <section className="links">
         <MenuNav menuLinks={menuLinks} />
       </section>
     </nav>
-  ) : null;
+  );
 };
 
 export default Nav;
