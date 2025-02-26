0804c69199695258c3555cd53cfe65cc69bb63ca
MortezaRasti <mortezamastercode@gmail.com>
2025-02-26 12:16:54 +0100

    Fixat mobile reponsivitet

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,21 +7,7 @@ import logo from "../assets/svg/logo.svg";
 const Landing = () => {
   const navigate = useNavigate();
   return (
-    <section className="landing-container" onClick={() => navigate("/menu")}>
-      <article className="landing-left">
-        <img src={leftImg} />
-      </article>
-
-      <article className="landing-center">
-        <img src={logo} />
-        <h1>AIR BEAN</h1>
-        <p>DRONEDELIVERED COFFEE </p>
-      </article>
-
-      <article className="landing-right">
-        <img src={rightImg} />
-      </article>
-    </section>
+    <section className="landing-container" onClick={() => navigate("/menu")}> </section>
   );
 };
 
